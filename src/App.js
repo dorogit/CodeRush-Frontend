e2b2424c2161c65ce91ebf0c6534969e10b00f26
Tushar Jain <tusharlock10@gmail.com>
2024-04-07 23:20:10 +0530

    wip

--- src/App.js
+++ src/App.js
@@ -1,12 +1,36 @@
 import Home from "./components/Home";
+import { useRef, useState,useEffect } from "react";
 import "./App.css"
 
 function App() {
+
+  const counterRef = useRef(null)
+  const [currentValue, setCurrentValue] = useState(0)
+
+  useEffect(()=> {
+    startLoader()
+  }, [])
+
+  const startLoader = () => {
+    const intervalId = setInterval(() => {
+      const randomIncrement = Math.floor(Math.random() * 10) + 1; // Generate random increment between 1 and 10
+      setCurrentValue(prevValue => {
+        const newValue = prevValue + randomIncrement;
+        return newValue > 100 ? 100 : newValue; // Cap the value at 100
+      });
+    }, 100); // Update every 0.1 second (100 milliseconds)
+
+    // Stop the interval once currentValue reaches 100
+    if (currentValue >= 100) {
+      clearInterval(intervalId);
+    }
+  };
+
   return (
     <div className="App">
       <div>
 
-        <h1 className="counter">0</h1>
+        <h1 ref={counterRef} className="counter">{currentValue}%</h1>
 
         <div className="overlay">
           <div className="bar"></div>
